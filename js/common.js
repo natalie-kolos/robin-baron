3a9fc87025bec4b7c3bdb2d15249259b945a4d78
Natalie <61272422+natalie-kolos@users.noreply.github.com>
2024-04-26 14:20:59 -0400

    Design Portfolio Page

--- js/common.js
+++ js/common.js
@@ -37,25 +37,28 @@ $(document).ready(function () {
   });
   $(".gallery-carousel").slick({
     dots: true,
+    arrows: true,
     infinite: false,
     speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 5,
+    slidesToScroll: 5,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow:4,
+          slidesToScroll: 4,
           infinite: true,
           dots: true,
+          arrows: false,
         },
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: false,
         },
       },
     ],
